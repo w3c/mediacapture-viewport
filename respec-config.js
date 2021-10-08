bc834ef9e246f285878e6e610d18008c7db5e644
Dominique Hazael-Massieux <dom@w3.org>
2021-10-08 10:18:06 +0200

    Add skeleton spec

---
+++ respec-config.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  specStatus:           "ED", // "base",
+  shortName:            "viewport-capture",
+  copyrightStart: "2021",
+  edDraftURI:           "https://w3c.github.io/mediacapture-viewport/",
+  editors:  [
+    { name: "Elad Alon", company: "Google", w3cid: 118124 },
+    { name: "Jan-Ivar Bruaroey", company: "Mozilla", w3cid: 79152}
+  ],
+
+  group: "webrtc",
+  xref: ["html", "infra", "permissions", "permissions-policy", "dom"],
+  wgPublicList: "public-webrtc",
+  github: "https://github.com/w3c/mediacapture-viewport/",
+
+  otherLinks: [
+    {
+      key: "Participate",
+      data: [
+        {
+          value: "Mailing list",
+          href: "https://lists.w3.org/Archives/Public/public-webrtc/"
+        }
+      ]
+    }
+  ]
+};
